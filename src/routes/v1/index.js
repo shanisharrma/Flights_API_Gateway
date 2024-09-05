1b4a7006c2fc67b254e559e000114cab422a7ad1
codershani <codershani13@gmail.com>
2024-09-05 18:41:14 +0530

    Added JWT authentication

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const { InfoController } = require("../../controllers");
 const userRoutes = require("./user-routes");
+const { AuthRequestMiddleware } = require("../../middlewares");
 
 const router = express.Router();
 
 router.use("/user", userRoutes);
 
-router.get("/info", InfoController.info);
+router.get("/info", AuthRequestMiddleware.checkAuth, InfoController.info);
 
 module.exports = router;
