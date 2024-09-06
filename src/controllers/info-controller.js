8b240fc37891d68d086563be82b1c85ecc70c2bf
codershani <codershani13@gmail.com>
2024-09-06 13:31:59 +0530

    Added rate limiter and proxy middleware

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -3,7 +3,7 @@ const { StatusCodes } = require("http-status-codes");
 const info = (req, res) => {
   return res.status(StatusCodes.OK).json({
     success: true,
-    msg: "API is live",
+    msg: "API Gateway API is live",
     error: {},
     data: {},
   });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 const express = require("express");
+const { rateLimit } = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const { ServerConfig, Logger } = require("./config");
 const apiRoutes = require("./routes");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(limiter);
+
+app.use(
+  "/flightsService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/bookingsService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+  })
+);
 
 app.use("/api", apiRoutes);
 
@@ -14,3 +38,13 @@ app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
   Logger.info("Successfully started the server", "root", { msg: "something" });
 });
+
+/**
+ * User
+ *  |
+ *  v
+ * localhost:3001 (API Gateway) --> localhost:4000/api/v1/bookings
+ *  |
+ *  v
+ * localhost:3000/api/v1/flights
+ */
